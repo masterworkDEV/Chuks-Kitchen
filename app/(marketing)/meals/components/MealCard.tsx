0d6d674c9c04d41c44854f473929eb896bb4300d
masterworkDEV <godwinkelvin92@yahoo.com>
2026-03-11 15:46:13 +0100

    new update

--- app/(marketing)/meals/components/MealCard.tsx
+++ app/(marketing)/meals/components/MealCard.tsx
@@ -1,9 +1,51 @@
 import { NextPage } from "next";
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 
-interface Props {}
+interface Props {
+  id: number;
+  image: StaticImageData;
+  name: string;
+  title: string;
+  description: string;
+  price: number;
+}
 
-const MealCard: NextPage<Props> = ({}) => {
-  return <div></div>;
+const MealCard: NextPage<Props> = ({
+  id,
+  image,
+  name,
+  price,
+  description,
+  title,
+}) => {
+  return (
+    <div className="border-2 border-white  bg-white  rounded-2xl h-96" key={id}>
+      <Link href={`/meals/${id}`} className="block h-2/4 rounded-t-2xl mb-3">
+        <Image
+          src={image.src}
+          alt={name}
+          className=" rounded-t-2xl h-full w-full"
+        />
+      </Link>
+      <div className="card-bottom px-4 my-3">
+        <h3 className="mb-2 text-text text-xl font-semibold">{name}</h3>
+        <p className="mb-6 text-text ">
+          {description.length < 70
+            ? description
+            : description.slice(0, 70).concat("...")}
+        </p>
+        <div className="flex justify-between">
+          <h3 className="text-text  font-semibold text-orange ">
+            ₦{price.toLocaleString()}
+          </h3>
+          <button className=" bg-orange px-6 py-3 rounded-lg text-white text-sm font-semibold">
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MealCard;
--- app/sections/ChefSpecial.tsx
+++ app/sections/ChefSpecial.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 // static data
 import { foodData } from "../data";
+import MealCard from "../(marketing)/meals/components/MealCard";
 
 const ChefSpecial = () => {
   // default section style
@@ -22,39 +23,13 @@ const ChefSpecial = () => {
         {foodData.length ? (
           foodData
             .map((food) => (
-              <div
-                className="border-2 border-white  bg-white  rounded-2xl h-96"
-                key={food.name}
-              >
-                <Link
-                  href={`/meals/${food.id}`}
-                  className="block h-2/4 rounded-t-2xl mb-3"
-                >
-                  <Image
-                    src={food.image}
-                    alt={food.name}
-                    className=" rounded-t-2xl h-full w-full"
-                  />
-                </Link>
-                <div className="card-bottom px-4 my-3">
-                  <h3 className="mb-2 text-text text-xl font-semibold">
-                    {food.name}
-                  </h3>
-                  <p className="mb-6 text-text ">
-                    {food.description.length < 70
-                      ? food.description
-                      : food.description.slice(0, 70).concat("...")}
-                  </p>
-                  <div className="flex justify-between">
-                    <h3 className="text-text  font-semibold text-orange ">
-                      ₦{food.price.toLocaleString()}
-                    </h3>
-                    <button className=" bg-orange px-6 py-3 rounded-lg text-white text-sm font-semibold">
-                      Add to cart
-                    </button>
-                  </div>
-                </div>
-              </div>
+              <MealCard
+                image={food.image.src}
+                id={food.id}
+                name={food.name}
+                description={food.description}
+                price={food.price}
+              />
             ))
             .slice(0, 6)
         ) : (
